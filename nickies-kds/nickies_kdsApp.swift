4f5a6c04902af08dc1d5c5984d8951b12a297af3
Nik Uzair <nikuzairsc@gmail.com>
2024-10-27 09:04:14 +0800

    Initial Commit

---
+++ nickies-kds/nickies_kdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  nickies_kdsApp.swift
+//  nickies-kds
+//
+//  Created by Nik Uzair on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct nickies_kdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
